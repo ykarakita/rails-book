12ccb4687de58abc482ad3a2fbd53100b86c5b6d
ykarakita <taf.749@gmail.com>
2018-12-01 15:23:39 +0900

    Implement creation email action and email template

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,2 +1,12 @@
 class TaskMailer < ApplicationMailer
+  default from: 'task_leaf@example.com'
+
+  def creation_email(task)
+    @task = task
+    mail(
+        subject: 'タスク作成完了メール',
+        to: 'user@example.com',
+        from: 'taskleaf@example.com'
+    )
+  end
 end
